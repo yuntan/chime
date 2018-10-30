e20134971d14df5a418bc07eafddba7ab85b0461
Yuto Tokunaga <yuntan.sub1@gmail.com>
2018-10-30 16:54:50 +0900

    rewrite timer api from `setTimeout` to `chrome.alarms`

--- background.js
+++ background.js
@@ -5,32 +5,44 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 function chime() {
-  const now = new Date();
-  const hour = now.getHours(), min = now.getMinutes();
-  let text = '';
-  if (min === 0) text = `${hour} o'clock`;
-  else if (min === 15) text = `quarter past ${hour}`;
-  else if (min === 30) text = `half past ${hour}`;
-  else if (min === 45) text = `quarter to ${hour + 1}`;
-  else text = `${hour}:${min < 10 ? `0${min}` : min}`;
+  const lang = 'en-US';
+  const text = genSpeechText(lang);
 
   const synth = window.speechSynthesis;
   const utter = new SpeechSynthesisUtterance();
   utter.text = text;
-  utter.lang = 'en-US';
+  utter.lang = lang;
   utter.rate = .8;
   synth.speak(utter);
-
-  setTimer();
 }
 
-function setTimer() {
+function genSpeechText(lang) {
+  const now = new Date();
+  const hour = now.getHours(), min = now.getMinutes();
+
+  // TODO: lang
+  if (min === 0) return `${hour} o'clock`;
+  else if (min === 15) return `quarter past ${hour}`;
+  else if (min === 30) return `half past ${hour}`;
+  else if (min === 45) return `quarter to ${hour + 1}`;
+  else return `${hour}:${min < 10 ? `0${min}` : min}`;
+}
+
+function setAlarm() {
   const now = new Date();
   const min = now.getMinutes(), sec = now.getSeconds();
   const delaySec = (15 - (min % 15) - 1) * 60 + (60 - sec);
-  // const delaySec = 60 - sec; # for debug
-  console.log(`setTimeout ${delaySec}sec`);
-  setTimeout(chime, delaySec * 1000);
+  // const delaySec = 60 - sec; // for debug
+  console.log(`setAlarm: delay ${delaySec} sec`);
+  const when = Date.now() + delaySec * 1000
+
+  // https://developer.chrome.com/extensions/alarms
+  chrome.alarms.create('chime', { when })
 }
 
-setTimer();
+setAlarm();
+
+chrome.alarms.onAlarm.addListener(alarm => {
+  chime();
+  setAlarm();
+});
