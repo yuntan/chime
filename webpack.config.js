f06c63a30b6d03394d3ffba9970ed05bc078c73e
Yuto Tokunaga <yuntan.sub1@gmail.com>
2022-01-31 10:33:40 +0900

    update build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ZipWebpackPlugin = require('zip-webpack-plugin');
+
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src/background.js'),
@@ -7,4 +10,20 @@ module.exports = {
     filename: 'background.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        'manifest.json',
+        'node_modules/webextension-polyfill/dist/browser-polyfill.min.js',
+        'options.js',
+        'options.html',
+        '_locales/**',
+        'icons/*.png',
+      ],
+    }),
+    new ZipWebpackPlugin({
+      path: __dirname,
+      filename: 'ext.zip',
+    }),
+  ],
 };
